d656c89a1d2cee6524c29403c9a439905c916d42
Michael Masry <michealeshak@gmail.com>
2020-06-19 23:32:44 +0200

    Day 5 - Michael

--- Main Package/Test.py
+++ Main Package/Test.py
@@ -1,41 +1,26 @@
 import numpy as np
 from _collections import deque
 
-def swap_bits(bit):
-    if bit == 0:
-        return 1
-    else:
-        return 0
 
-def shiftbit(array,direction):
-    items=deque(array)
+def shift_bit(array, direction):
+    items = deque(array)
     items.rotate(direction)
     return items
 
+
 def get_neighbor_combinations(k):
-    old_shape = k.shape
-    neighbor_combinations = np.empty(shape=(np.size(k),), dtype=object)
-    count=0
-    temp = k.copy()
-    for i in range(np.size(k,0)):
-        for j in range(np.size(k,1)-1):
-            temp[i] = shiftbit(temp[i],1)
-            print(temp[i])
-            neighbor_combinations[count] = temp
+    size = (np.size(k, 1) - 1) * np.size(k, 0)
+    neighbor_combinations = np.empty(shape=(size,), dtype=object)
+    count = 0
+    for i in range(np.size(k, 0)):
+        temp = k.copy()
+        for j in range(np.size(k, 1)-1):
+            temp[i] = shift_bit(temp[i], 1)
+            neighbor_combinations[count] = temp.copy()
             count += 1
     return neighbor_combinations
 
 
+aaa = np.arange(600).reshape(100, 6)
 a = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
-# print(a)
-# print("###########################################################################################")
-r = get_neighbor_combinations(a)
-print("###########################################################################################")
-print(r)
-# print(r)
-# print(np.size(a,0),np.size(a,0))
-# for i in range(np.size(a,0)):
-#     for j in range(np.size(a,0)-1):
-#         a[i]=shiftbit(a[i],1)
-#
-# print(a)
+r = get_neighbor_combinations(aaa)
