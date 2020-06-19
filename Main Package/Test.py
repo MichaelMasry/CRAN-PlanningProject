4ce3d3d45d27da535b21ba72e419ea8afb27e774
karim <keemolandz@gmail.com>
2020-06-19 22:37:14 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/CRAN-PlanningProject.iml
    #	.idea/misc.xml

--- Main Package/Test.py
+++ Main Package/Test.py
@@ -1,6 +1,5 @@
-import itertools
 import numpy as np
-
+from _collections import deque
 
 def swap_bits(bit):
     if bit == 0:
@@ -8,19 +7,35 @@ def swap_bits(bit):
     else:
         return 0
 
+def shiftbit(array,direction):
+    items=deque(array)
+    items.rotate(direction)
+    return items
 
 def get_neighbor_combinations(k):
     old_shape = k.shape
     neighbor_combinations = np.empty(shape=(np.size(k),), dtype=object)
-    for i in range(np.size(k)):
-        temp = k.copy()
-        temp = np.reshape(temp, (1, np.size(k)))
-        temp[0, i] = swap_bits(temp[0, i])
-        temp = np.reshape(temp, old_shape)
-        neighbor_combinations[i] = temp
+    count=0
+    temp = k.copy()
+    for i in range(np.size(k,0)):
+        for j in range(np.size(k,1)-1):
+            temp[i] = shiftbit(temp[i],1)
+            print(temp[i])
+            neighbor_combinations[count] = temp
+            count += 1
     return neighbor_combinations
 
 
-a = np.array([[0, 1, 0], [1, 0, 0], [0, 1, 0]])
-print(a)
+a = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+# print(a)
+# print("###########################################################################################")
 r = get_neighbor_combinations(a)
+print("###########################################################################################")
+print(r)
+# print(r)
+# print(np.size(a,0),np.size(a,0))
+# for i in range(np.size(a,0)):
+#     for j in range(np.size(a,0)-1):
+#         a[i]=shiftbit(a[i],1)
+#
+# print(a)
